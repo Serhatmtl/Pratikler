4845dc613ff0d5d37ff0c9c29f50fe72c3864959
Serhat MUTLU <60172407+Serhatmtl@users.noreply.github.com>
2022-01-21 11:07:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console_proramlama
+{
+
+
+    class Program
+    {
+        public static void Main(String [] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
+
+
